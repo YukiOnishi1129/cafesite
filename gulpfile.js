cf5b5f37e8a8be9cedb37dd6800052e366406473
YukiOnishi1129 <>
2019-11-28 17:26:20 +0900

    gulp 画像圧縮監視プロセス追加

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,9 @@ const sass = require('gulp-sass');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
 const babel = require('gulp-babel');
+const imagemin = require("gulp-imagemin");
+const pngquant = require("imagemin-pngquant");
+const mozjpeg = require("imagemin-mozjpeg");
 
 // ローカルサーバーの立ち上げ
 const browserSyncOption = {
@@ -23,7 +26,7 @@ function sync(done) {
 }
 
 // sassのコンパイル
-gulp.task('sass', () => {
+gulp.task("sass", () => {
     return gulp
         .src('scss/*.scss')
         .pipe(sass())
@@ -36,12 +39,29 @@ gulp.task("babel", () => {
       .pipe(babel({
         "presets": ["@babel/preset-env"]
       }))
+    //minify(圧縮)
       .pipe(uglify())
+    // リネーム
       .pipe(rename({
-        extname: '.min.js'
+        extname: '.min.js' 
       }))
       .pipe(gulp.dest('js/dist'));
 });
+
+// 画像の圧縮
+gulp.task("imagemin", () => {
+    return gulp.src('img/src/*.{jpg,jpeg,png,gif,svg}')
+    .pipe(imagemin(
+        [
+            pngquant({ quality: '65-80', speed: 1 }),
+            mozjpeg({ quality: 80 }),
+            imagemin.svgo(),
+            imagemin.gifsicle()
+        ]
+    ))
+    .pipe(gulp.dest('img/min'));
+});
+
 
 // 監視プロセス
 function watchFiles(done) {
@@ -49,10 +69,11 @@ function watchFiles(done) {
       browserSync.reload();
       done();
     };
-    gulp.watch('scss/*.scss').on('change', gulp.series('sass', browserReload));
+    gulp.watch('scss/*.scss').on('change', gulp.series("sass", browserReload));
     gulp.watch('js/src/*.js').on('change', gulp.series("babel", browserReload));
     gulp.watch('index.html').on('change', gulp.series(browserReload));
+    gulp.watch('img/src/*.{jpg,jpeg,png,gif,svg}').on('change', gulp.series("imagemin", browserReload));
 }
 
 // 「npx gulp」にて監視プロセスを実行(sass, babel, 画面リロードを実行)
-gulp.task('default', gulp.series(gulp.parallel('sass', "babel"), gulp.series(sync, watchFiles)));
+gulp.task('default', gulp.series(gulp.parallel("sass", "babel", "imagemin"), gulp.series(sync, watchFiles)));
